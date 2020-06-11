095c3613e8653aaec46c0b49dc0731b6de691f1b
junukim <junukim.dev@gmail.com>
2020-06-11 16:10:33 +0900

    [CREATE] infinity scroll

---
+++ src/views/infinityScroll/index.tsx
@@ -0,0 +1,36 @@
+import React, { FC } from "react";
+import InfiniteScroll from "react-infinite-scroller";
+
+import { Loading } from "../../assets";
+import * as S from "./style";
+
+interface OwnProps {
+  loadMore: () => void;
+  hasMore: boolean;
+}
+
+const LoadingBar: FC = () => {
+  return (
+    <S.Loading key="loading">
+      <Loading />
+    </S.Loading>
+  );
+};
+
+const InfinityScroll: FC<OwnProps> = ({ loadMore, hasMore, children }) => {
+  return (
+    <InfiniteScroll
+      pageStart={0}
+      loadMore={loadMore}
+      loader={<LoadingBar />}
+      hasMore={hasMore}
+      useWindow={false}
+      isReverse={true}
+      threshold={10}
+    >
+      {children}
+    </InfiniteScroll>
+  );
+};
+
+export default InfinityScroll;
---
+++ src/views/infinityScroll/style/index.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Loading = styled.div`
+  width: 100%;
+  padding: 2px 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  > svg {
+    width: 20px;
+    height: 20px;
+  }
+`;
