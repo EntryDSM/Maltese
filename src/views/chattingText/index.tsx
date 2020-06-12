33ebd3e8180c14bec5a8f9a45149a549c896b319
junukim <junukim.dev@gmail.com>
2020-06-12 19:21:41 +0900

    [UPDATE] chat bubble

--- src/views/chattingText/index.tsx
+++ src/views/chattingText/index.tsx
@@ -1,38 +1,76 @@
 import React, { FC, useState, useCallback } from "react";
 
+import type { User } from "../../data/apiTypes";
 import InfinityScroll from "../infinityScroll";
 import * as S from "./style/index";
 
 const ChattingText: FC = () => {
-  const [a, sA] = useState<any[]>([]);
+  const [data, setData] = useState<User[] | null>(
+    null
+    //   [
+    //   {
+    //     qna_id: 1,
+    //     admin_email: "admin@example.com",
+    //     user_email: "user@example.com",
+    //     to: "admin",
+    //     content: "안녕하세요",
+    //     created_at: "2020-06-03T05:12:40.000Z",
+    //   },
+    //   {
+    //     qna_id: 2,
+    //     admin_email: "admin@example.com",
+    //     user_email: "user@example.com",
+    //     to: "student",
+    //     content:
+    //       "반갑습니반갑습니반갑습니반갑습니반갑습니반갑습니반갑습니반갑습니반갑습니반갑습니반갑습니반갑습니반갑습니반갑습니반갑습니",
+    //     created_at: "2020-06-03T05:13:40.000Z",
+    //   },
+    //   {
+    //     qna_id: 3,
+    //     admin_email: "admin@example.com",
+    //     user_email: "user@example.com",
+    //     to: "admin",
+    //     content: "안녕하세요",
+    //     created_at: "2020-06-03T05:15:40.000Z",
+    //   },
+    // ]
+  );
   const [page, setPage] = useState(0);
   const [hasMore, setHasMore] = useState(true);
 
   const fetchMoreData = useCallback(async () => {
-    if (a.length > 1000) setHasMore(false);
+    if (data === null) {
+      setHasMore(false);
+      return;
+    }
+    if (data.length > 50) setHasMore(false);
 
-    await sA([
-      page,
-      page,
-      page,
-      page,
-      page,
-      page,
-      page,
-      page,
-      page,
-      page,
-      ...a,
+    await setData([
+      {
+        qna_id: page + 4,
+        admin_email: "admin@example.com",
+        user_email: "user@example.com",
+        to: "admin",
+        content: "안녕하세요",
+        created_at: "2020-06-03T05:15:40.000Z",
+      },
+      ...data,
     ]);
     await setPage(page + 1);
-  }, [a, page]);
+  }, [data, page]);
 
   return (
     <S.Wrapper>
       <InfinityScroll loadMore={fetchMoreData} hasMore={hasMore}>
-        {a.map((v, i) => (
-          <div key={i.toString()}>{v}</div>
-        ))}
+        {data === null || data.length === 0 ? (
+          <S.EmptyData>EntryDSM에 궁금한점이 있으신가요?</S.EmptyData>
+        ) : (
+          data.map((v) => (
+            <S.ChatBubble key={v.qna_id} isAdmin={v.to === "admin"}>
+              <p>{v.content}</p>
+            </S.ChatBubble>
+          ))
+        )}
       </InfinityScroll>
     </S.Wrapper>
   );
--- src/views/chattingText/style/index.ts
+++ src/views/chattingText/style/index.ts
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-// import { Thema } from "../../../Thema";
+import { Thema } from "../../../Thema";
 
 export const Wrapper = styled.div`
   flex: 1;
@@ -12,4 +12,35 @@ export const Wrapper = styled.div`
     width: 100%;
     height: 100%;
   }
+`;
+
+export const ChatBubble = styled.div<{ isAdmin: boolean }>`
+  width: 100%;
+  margin-top: 4px;
+  display: flex;
+  justify-content: ${({ isAdmin }) => (isAdmin ? "flex-end" : "flex-start")};
+
+  > p {
+    margin: 0;
+    width: fit-content;
+    max-width: 280px;
+    min-height: 36px;
+    padding: 10px 12px;
+    box-sizing: border-box;
+    font-size: 14px;
+    border-radius: ${Thema.DEFAULT_RADIUS};
+    border-bottom-left-radius: ${({ isAdmin }) => !isAdmin && 0};
+    border-bottom-right-radius: ${({ isAdmin }) => isAdmin && 0};
+    background: ${({ isAdmin }) =>
+      isAdmin ? Thema.MAIN_COLOR_1 : Thema.SERVE_COLOR_1};
+    color: ${({ isAdmin }) =>
+      isAdmin ? Thema.MAIN_COLOR_2 : Thema.SERVE_COLOR_2};
+  }
+`;
+
+export const EmptyData = styled.p`
+  width: 100%;
+  text-align: center;
+  font-size: 14px;
+  color: ${Thema.MAIN_COLOR_1};
 `;
