72949e727c0c998103d218dfa18511ca66cb3dab
junukim <junukim.dev@gmail.com>
2020-08-08 15:36:31 +0900

    [UPDATE] add apis & sockets

--- src/data/apis.ts
+++ src/data/apis.ts
@@ -1,7 +1,16 @@
 import axios, { AxiosError } from "axios";
-import type { AccessToken, User } from "./apiTypes";
+import type { AccessToken, User, PagenationPayload } from "./apiTypes";
+import { baseURL } from "./endpoints";
 
-const baseURL = "https://api.entrydsm.hs.kr";
+export const responseStatus = (status: number) => {
+  return {
+    _200: status === 200,
+    _400: status === 400,
+    _401: status === 401,
+    _403: status === 403,
+    _404: status === 404,
+  };
+};
 
 const authorizationHeader = (accessToken: string) => ({
   Authorization: `Bearer ${accessToken}`,
@@ -12,16 +21,19 @@ const instanceAxios = axios.create({
   headers: { "Content-Type": "application/json" },
 });
 
-export const getChatsApi = async (payload: AccessToken) => {
+export const getChatsApi = async (payload: AccessToken & PagenationPayload) => {
   try {
-    const response = await instanceAxios.get<User>("/schnauzer/chats", {
+    const response = await instanceAxios.get<User[]>("/v5/qna/chats", {
       headers: authorizationHeader(payload.accessToken),
+      params: {
+        page: payload.page,
+      },
     });
 
-    return [response.data, response.status];
+    return { data: response.data, status: response.status };
   } catch (_err) {
-    const err: AxiosError = _err;
+    const err = _err as AxiosError<null>;
 
-    return [null, err.response.status];
+    return { data: [] as User[], status: err.response?.status || 0 };
   }
 };
---
+++ src/data/endpoints.ts
@@ -0,0 +1,1 @@
+export const baseURL = "https://schnauzer.entrydsm.hs.kr";
