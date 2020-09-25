4d71cc157c3d1c6aac827c6ffe5934495dfd59bd
junukim <junukim.dev@gmail.com>
2020-09-25 19:06:44 +0900

    [UPDATE] text break bug

--- dist/views/chatting/style/index.d.ts
+++ dist/views/chatting/style/index.d.ts
@@ -1,2 +1,4 @@
-export declare const Wrapper: import("styled-components").StyledComponent<"div", any, {}, never>;
+export declare const Wrapper: import("styled-components").StyledComponent<"div", any, {
+    isOpen: boolean;
+}, never>;
 export declare const MessageView: import("styled-components").StyledComponent<"div", any, {}, never>;
